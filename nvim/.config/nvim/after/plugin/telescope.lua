94cd174e3fb61b9e38b5430d9c8b4c345255cfc9
Dvip1 <dvipatwork@outlook.com>
2024-10-26 21:27:13 +0530

    some changes

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,7 +1,21 @@
+local telescope = require('telescope')
+
+telescope.setup({
+    defaults = {
+        follow = true,  -- Follow symbolic links
+        hidden = true,  -- Show hidden files
+    },
+    pickers = {
+        find_files = {
+            follow = true,
+            hidden = true
+        }
+    }
+})
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({search = vim.fn.input("Grep > ")})
 end, { desc = 'Telescope grep string' })
-
